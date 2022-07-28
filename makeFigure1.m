f23751732e1c273b8ecdd0b7125d87cfe49b17af
benp <benpolletta@gmail.com>
2022-07-28 16:19:40 -0400

    Files for making Figure 1.

---
+++ makeFigure1.m
@@ -0,0 +1,17 @@
+function sim_struct = makeFigure1
+
+[model_vary, model_names, include_deepFS] = getModelParams;
+
+for m = 1:length(model_vary)
+    
+    Iapp=0:.1:20;
+
+    simulation_vary = {'deepRS', 'Iapp', Iapp};
+    
+    vary = [model_vary{m}; simulation_vary];
+    
+    sim_struct{m} = initSimStruct('vary', vary, 'name', model_names{m}, 'include_deepFS', include_deepFS(m));
+    
+    simulateModel(sim_struct{m})
+    
+end
